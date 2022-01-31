5434914145f3ce2d5877e18414245a0dfbe22be4
DestinyX2-Home <crvenegas17@alumnos.utalca.cl>
2022-01-31 16:28:51 -0300

    billboard changes

--- ReadyPlayerMe Testing/Assets/Billboard.cs
+++ ReadyPlayerMe Testing/Assets/Billboard.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    private GameObject target;
+    private Transform target;
     private Vector3 offset;
-
+    private bool canTurn = false;
     // Start is called before the first frame update
     void Start()
     {
-        target = Camera.main.gameObject;
+        target = Camera.main.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target.transform);
+        if(canTurn) 
+            transform.LookAt(new Vector3(target.position.x, transform.position.y,target.position.z));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            canTurn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canTurn = false;
+        }
     }
 }
